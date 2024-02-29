4506bca55b6ed1d2ac5c09fbbcac6f85dba5df5f
apodacaenzo99 <apodacaenzo99@gmail.com>
2024-02-29 12:20:10 -0300

    Guardando cambios

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+//TODO : se podría bloquear el botón mientras no haya 5 letras
+//TODO: https://random-word-api.herokuapp.com/word?length=5&lang=es
+// https://random-word-api.vercel.app/
+// TODO: en input evitar que ingresen numeros
+
 const V = '#79b851';
 const A = '#f3c237';
 const G = '#a4aec4';
 
-const PALABRA = "APPLE";
+let palabra = "";
 let tieneVidas = 6;
+let listado = []; // historico de intentos
+let diccionario = { "clave": "valor" };
+
+fetch("https://random-word-api.herokuapp.com/word?length=5&lang=es")
+    .then(response => response.json())
+    .then(response => {
+        console.log(response[0]);
+        palabra = response[0].toUpperCase();
+    })
+    .catch(err => {
+        // incluir metodo para obtener palabra de lista offline
+        console.error(err);
+    });
+
 
 const INPUT = document.getElementById("guess-input");
 const BOTON = document.getElementById("guess-button");
@@ -12,13 +31,13 @@ const GRILLA = document.getElementById("grid");
 
 BOTON.addEventListener("click", () => {
     const INTENTO = leerIntento();
-    if (PALABRA === INTENTO) {
+    if (palabra === INTENTO) {
         terminar("Ganaste!");
         // TODO: mostrar el mensaje
         // TODO: volver a empezar (restaurar vidas, cambiar palabra, vaciar intentos)
         return;
     }
-    verificar(PALABRA, INTENTO);
+    verificar(palabra, INTENTO);
 });
 
 function crearLetraEnGrilla(row, letra, color) {
@@ -31,12 +50,18 @@ function crearLetraEnGrilla(row, letra, color) {
 }
 
 function leerIntento() {
-    return INPUT.value.toUpperCase();
+    return INPUT.value.toUpperCase().trim();
 }
 
 function verificar(palabra, intento) {
+    console.log("Verificando:", intento);
+    if (intento.length != 5) {
+        alert("La palabra debe tener 5 caracteres");
+        return;
+    }
     const row = document.createElement("div");
     row.className = "row";
+    listado.push(intento);
     for (const i in intento) {
         if (intento[i] == palabra[i]) {
             crearLetraEnGrilla(row, intento[i], V);
@@ -51,6 +76,7 @@ function verificar(palabra, intento) {
         // TODO: mostrar el mensaje
         // TODO: volver a empezar (restaurar vidas, cambiar palabra, vaciar intentos)
         // TODO: 
+
         terminar("Perdiste!");
         return;
     }
@@ -58,5 +84,6 @@ function verificar(palabra, intento) {
 
 function terminar(msg) {
     tieneVidas = 6;
+    listado = [];
     AVISO.innerHTML = "<h1>" + msg + "</h1>";
 }
